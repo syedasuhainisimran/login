c96426ce30ff8f26531382e1b04899a90c63b7d2
syedasuhainisimran <syedasuhaini@gmail.com>
2019-09-23 20:34:06 +0600

    Our first Work

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php 
+$connect = mysqli_connect('localhost','root','','login');
+
+$sql = "INSERT INTO adds(fast_name,last_name,email,password,textarea) VALUES('$first','$last','$email','$password','$text')";
+
+$mysql_query = mysqli_query($connect, $sql);
+if ($mysql_query) {
+	echo "data inser successfully";
+}else{
+	die('data not insert'.mysqli_error($connect));
+}
+
+
+
