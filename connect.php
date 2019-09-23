ed5be43ec38493527be001fb063f88433d52944c
syedasuhainisimran <syedasuhaini@gmail.com>
2019-09-23 20:47:53 +0600

    test code

--- connect.php
+++ connect.php
@@ -9,6 +9,16 @@ if ($mysql_query) {
 }else{
 	die('data not insert'.mysqli_error($connect));
 }
+$connect = mysqli_connect('localhost','root','','login');
+
+$sql = "INSERT INTO adds(fast_name,last_name,email,password,textarea) VALUES('$first','$last','$email','$password','$text')";
+
+$mysql_query = mysqli_query($connect, $sql);
+if ($mysql_query) {
+	echo "data inser successfully";
+}else{
+	die('data not insert'.mysqli_error($connect));
+}
 
 
 
